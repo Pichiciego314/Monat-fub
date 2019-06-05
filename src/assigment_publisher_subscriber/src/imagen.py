76a9c47a7ca9d3c995bcf0ee64494c140f5c3058
Pichiciego314 <49697801+Pichiciego314@users.noreply.github.com>
2019-06-05 15:16:16 -0500

    Update imagen.py

--- src/assigment_publisher_subscriber/src/imagen.py
+++ src/assigment_publisher_subscriber/src/imagen.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+# Here the libraries are imported
 import numpy as np
 import rospy
 import tf
@@ -9,38 +9,40 @@ from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
-class parking:
+#here "image" is captured
+class image:
+    #Here are the permanent variables
     def __init__(self):
         self.net_image = []
-        self.permanente = 0  # Variable de vida permanente
-        self.camara_sub = rospy.Subscriber("/sensors/camera/color/image_raw", Image, self.get_image)
-        self.img_pub = rospy.Publisher("/imagen/nuevo_topico", Image, queue_size=10)
-        self.bridge = CvBridge()
+        self.camara_sub = rospy.Subscriber("/sensors/camera/color/image_raw", Image, self.get_image)#here is the subscription to the camera topic
+        self.img_pub = rospy.Publisher("/imagen/nuevo_topico", Image, queue_size=10)#I publish the new image into a new topic
+        self.bridge = CvBridge()#the funcition that will convert the images
 
-    def get_image(self, mensaje_recibido):
+    def get_image(self, recived_message):
         try:
-            self.net_image = self.bridge.imgmsg_to_cv2(mensaje_recibido)
+            self.net_image = self.bridge.imgmsg_to_cv2(recived_message)
         except CvBridgeError as e:
             print(e)
 
-    def simple_parking(self):
+    def image_converter(self):
         if self.net_image == []:
             return
 
         try:
+            #here are the convertions of the image into something the computer can read
             ros_img = self.bridge.cv2_to_imgmsg(self.net_image)
             self.img_pub.publish(ros_img)
         except CvBridgeError as e:
             print(e)
 
-
+#definition of main function
 if __name__ == '__main__':
     try:
-        rospy.init_node('imagen')
-        dummy = parking() # Crea un objeto para ejecutar parking
+        rospy.init_node('imagen')# node that is going to be initialize
+        dummy = image() # creates object to execute parking
         rate = rospy.Rate(10) # Que tan rapido quiero que se ejecute el programa
         while not rospy.is_shutdown():
-            dummy.simple_parking()
+            dummy.image_converter()
             rate.sleep()
     except rospy.ROSInterruptException:
         print("Algo raro esta pasando")
