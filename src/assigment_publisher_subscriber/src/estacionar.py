472ddcb5c466625f97f9d48fa6c65a67843b3e0f
Pichiciego314 <49697801+Pichiciego314@users.noreply.github.com>
2019-06-04 21:51:26 -0500

    Update estacionar.py

--- src/assigment_publisher_subscriber/src/estacionar.py
+++ src/assigment_publisher_subscriber/src/estacionar.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-
+#here are impoting the libraries the program will be used
 import numpy as np
 import rospy
 import tf
@@ -10,59 +10,57 @@ from nav_msgs.msg import Odometry
 
 
 class parking:
+    #Defiition of permanent variables
     def __init__(self):
+        # The subscribers for the topic of the car
         self.vel_pub = rospy.Publisher("/control/command/normalized_wanted_speed", NormalizedSpeedCommand, queue_size=0)
         self.dir_pub = rospy.Publisher("/control/command/normalized_wanted_steering", NormalizedSteeringCommand, queue_size=0)
 
     def simple_parking(self):
-
-        # Definiendo mensaje de velocidad
+        # here comes the values I will asign to the topics
+        # definition of message for speed
         velocidad = NormalizedSpeedCommand()
 
 
-        # Definiendo mensaje de direccion
+        # definition of message for steering
         direccion = NormalizedSteeringCommand()
 
         rospy.sleep(1.0)
-
-        ''' Aqui va la magia de seleccionar que 
-        direccion y vel se desea (velocidad.value = ?, direccion.value = ?) '''
 #here are the values assigned to speed amd steering
-        # Primeros valores
+        # first values
         velocidad.value = -0.2
         direccion.value = 0.7
         self.vel_pub.publish(velocidad)
         self.dir_pub.publish(direccion)
-        # Parar tiempo
+        # use this values for this period of time
         rospy.sleep(2.0)
-        # Segundos valores
+        # second values
         velocidad.value = -0.2
         direccion.value = -0.7
-        #self.vel_pub.publish(velocidad)
+        self.vel_pub.publish(velocidad)
         self.dir_pub.publish(direccion)
-        # Parar tiempo
+        # use this values for this period of time
         rospy.sleep(1.5)
-        #terceros valores
+        #third values
         velocidad.value = 0.2
         direccion.value = 0.2
         self.vel_pub.publish(velocidad)
         self.dir_pub.publish(direccion)
         rospy.sleep(1.0)
         
-
-#para parar
+        # for stop, in this program the stop was by using ctrl+c
         velocidad.value = 0.0
         direccion.value = 0.0
         self.vel_pub.publish(velocidad)
         self.dir_pub.publish(direccion)
         rospy.sleep(10.0)
 
-
+# main function
 if __name__ == '__main__':
     try:
         rospy.init_node('estacionar')
-        hola = parking() # Crea un objeto para ejecutar parking
-        rate = rospy.Rate(10) # Que tan rapido quiero que se ejecute el programa
+        hola = parking() # create an object to execute parking
+        rate = rospy.Rate(10) # how fast the program will be executed
         while not rospy.is_shutdown():
             hola.simple_parking()
             rate.sleep()
